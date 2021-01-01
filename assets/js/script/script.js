072b9e98948a8bd65a292354004196754240b22d
Stepan Lyubavtsev <nameasy@mail.ru>
2021-01-01 14:16:42 +0300

    Add files via upload

--- assets/js/script/script.js
+++ assets/js/script/script.js
@@ -1,14 +1,52 @@
+var palette = ["60b2cc", "eabc32", "c96565", "7f5b93", "53ae9d"],
+    random = Math.floor(Math.random() * palette.length),
+    $random = Math.floor(Math.random() * palette.length);
 $(document).ready(function () {
-    function e() {
+    function t() {
         $(document).ready(function () {
-            let e = $("<style/>"),
-                t = "::-moz-selection {color:#fff;background:#" + o[n] + "}::selection{color:#fff;background:#" + o[n] + "}";
-            $(e).html(t.replace(/palette/g, o)), $("body").append(e)
+            var t = $("<style/>"),
+                e = "::-moz-selection {color:#fff;background:#" + palette[random] + "}::selection{color:#fff;background:#" + palette[random] + "}";
+            $(t).html(e.replace(/palette/g, palette)), $("body").append(t)
         })
     }
-    let o = ["60b2cc", "eabc32", "c96565", "7f5b93", "53ae9d"],
-        n = Math.floor(Math.random() * o.length);
-    Math.random(), o.length, $(window).on("resize", function () {
-        feather.replace(), e()
-    }), feather.replace(), e()
+
+    function e() {
+        var t = $(".projects-grid .project").width(),
+            e = $(".projects-grid .project img").width();
+        $(".projects-grid .project").css({
+            height: .75 * t + 4
+        }), $(".projects-grid .project img").css({
+            height: .75 * e
+        })
+    }
+
+    function o() {
+        var t;
+        t = $("body").width(), 640 > $("body").width() ? $(".grid .grid__item").css({
+            width: t - 18 - 18
+        }) : 960 > $("body").width() ? $(".grid .grid__item").css({
+            width: (t - 18 - 18) / 2
+        }) : $(".grid .grid__item").css({
+            width: (t - 18 - 18) / 4
+        })
+    }
+
+    function i() {
+        r.each(function () {
+            var t = $(this).attr("id"),
+                e = $('ul.navigation li.anchor-link a[href="#' + t + '"]');
+            $(this).offset().top - $(window).height() / 2 < $(window).scrollTop() && $(this).offset().top + $(this).height() - $(window).height() / 2 > $(window).scrollTop() ? e.addClass("current") : e.removeClass("current")
+        })
+    }
+    var r = $(".wonderful-section");
+    $("ul.navigation li.anchor-link").on("click", "a", function (t) {
+        var e;
+        t.preventDefault(), e = $(this.hash), $("body,html").animate({
+            scrollTop: e.offset().top
+        }, 250)
+    }), $(window).on("resize", function () {
+        feather.replace(), t(), e(), o()
+    }), feather.replace(), t(), e(), o(), $(window).on("scroll", function () {
+        i()
+    }), i()
 });
