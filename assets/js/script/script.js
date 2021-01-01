0be608822b7b02053d6e8b28e451e9978d78b14a
Stepan Lyubavtsev <nameasy@mail.ru>
2021-01-01 13:46:51 +0300

    Add files via upload

---
+++ assets/js/script/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    function e() {
+        $(document).ready(function () {
+            let e = $("<style/>"),
+                t = "::-moz-selection {color:#fff;background:#" + o[n] + "}::selection{color:#fff;background:#" + o[n] + "}";
+            $(e).html(t.replace(/palette/g, o)), $("body").append(e)
+        })
+    }
+    let o = ["60b2cc", "eabc32", "c96565", "7f5b93", "53ae9d"],
+        n = Math.floor(Math.random() * o.length);
+    Math.random(), o.length, $(window).on("resize", function () {
+        feather.replace(), e()
+    }), feather.replace(), e()
+});
